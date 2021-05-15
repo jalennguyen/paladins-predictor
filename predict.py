f80383bdb4d108f8dc2e1426cf4775d329e3eeca
Jalen Nguyen <jalennguyen@gmail.com>
2021-05-15 03:33:26 -0300

    Updated predict.py

--- predict.py
+++ predict.py
@@ -1,22 +1,60 @@
-import pickle
+from dotenv import load_dotenv
+import os
 from pyrez.api import PaladinsAPI
-from scrape import get_champ
+import pickle
 
-# authorize with paladins api
-paladins = PaladinsAPI(devId=0000, authKey="0000")
+# load in environment variables
+load_dotenv()
+DEV_ID = os.getenv("DEVELOPER_ID")
+AUTH_KEY = os.getenv("AUTHENTICATION_KEY")
+
+# create paladins object
+paladins = PaladinsAPI(devId=DEV_ID, authKey=AUTH_KEY)
 
 
-def live_team_stats(team, i):
+def get_champ(player_id, champ_id):
+    # retrieve player's stats for each champion
+    champions = paladins.getQueueStats(player_id, 486)
+
+    # retrieve stats for current champion
+    for champion in champions:
+        if champion["ChampionId"] == champ_id:
+            return champion
+
+    # return blank stats if first time playing champion
+    empty = {
+            "Assists": 0,
+            "Champion": None,
+            "ChampionId": champ_id,
+            "Deaths": 0,
+            "Gold": 0,
+            "Kills": 0,
+            "LastPlayed": None,
+            "Losses": 0,
+            "Matches": 0,
+            "Minutes": 0,
+            "Queue": "",
+            "Wins": 0,
+            "player_id": str(player_id),
+            "ret_msg": None
+        }
+
+    return empty
+
+
+def raw_live_stats(team, i):
     stats = {
         "match_id": team[0]["Match"]
     }
     for player in team:
+        # collect account stats
         stats[f"a_lvl{i}"] = player["Account_Level"]
         stats[f"tier{i}"] = player["Tier"]
         stats[f"wins{i}"] = player["tierWins"]
         stats[f"losses{i}"] = player["tierLosses"]
         stats[f"champ{i}"] = player["ChampionId"]
 
+        # collect champion stats
         champ = get_champ(player["playerId"], player["ChampionId"])
         stats[f"assists{i}"] = champ["Assists"]
         stats[f"deaths{i}"] = champ["Deaths"]
@@ -29,11 +67,7 @@ def live_team_stats(team, i):
     return stats
 
 
-def live_stats(match_id, player_id):
-    # retrieve info on live match
-    match = paladins.getMatch(match_id, True)
-
-    side = 0
+def live_stats(match):
     team1 = []
     team2 = []
     # split players into their respective teams
@@ -41,13 +75,11 @@ def live_stats(match_id, player_id):
         if player["taskForce"] == 1:
             team1.append(player)
         else:
-            if player["playerId"] == str(player_id):
-                side = 1
             team2.append(player)
 
     # save match info
     match = {
-        "match_id": match_id,
+        "match_id": match[0]['Match'],
         "winner": -1,
         "map": team1[0]["mapGame"],
         "region": team1[0]["playerRegion"],
@@ -55,121 +87,91 @@ def live_stats(match_id, player_id):
     }
 
     # get player stats for each team
-    stats1 = live_team_stats(team1, 1)
-    stats2 = live_team_stats(team2, 6)
+    stats1 = raw_live_stats(team1, 1)
+    stats2 = raw_live_stats(team2, 6)
 
-    return [match, stats1, stats2], side
+    return [match, stats1, stats2]
 
 
-def summed_wr(live_match):
-    # process raw stats for team1
+def sums(team, i):
+    sum_awr = 0
+    sum_ckda = 0
+    sum_cwr = 0
+
+    # take sums of account w/r, champion w/r, and champion kda for the team
+    for i in range(i, i + 5):
+        matches_played = team[f"wins{i}"] + team[f"losses{i}"]
+        if matches_played != 0:
+            sum_awr += team[f"wins{i}"] / matches_played
+
+        if team[f"deaths{i}"] == 0:
+            sum_ckda += team[f"kills{i}"] + 0.5 * team[f"assists{i}"]
+        else:
+            sum_ckda += (team[f"kills{i}"] + 0.5 * team[f"assists{i}"]) / team[f"deaths{i}"]
+
+        if team[f"c_matches{i}"] != 0:
+            sum_cwr += team[f"c_wins{i}"] / team[f"c_matches{i}"]
+
+    return sum_awr, sum_ckda, sum_cwr
+
+
+def process_features(live_match):
     team1 = live_match[1]
-    sum_awr1 = 0
-    sum_ckda1 = 0
-    sum_cwr1 = 0
-    for i in range(1, 6):
-        matches_played = team1[f"wins{i}"] + team1[f"losses{i}"]
-        if matches_played != 0:
-            sum_awr1 += team1[f"wins{i}"] / matches_played
-
-        if team1[f"deaths{i}"] == 0:
-            sum_ckda1 += team1[f"kills{i}"] + 0.5 * team1[f"assists{i}"]
-        else:
-            sum_ckda1 += (team1[f"kills{i}"] + 0.5 * team1[f"assists{i}"]) / team1[f"deaths{i}"]
-
-        if team1[f"c_matches{i}"] != 0:
-            sum_cwr1 += team1[f"c_wins{i}"] / team1[f"c_matches{i}"]
-
-    # process raw stats for team2
     team2 = live_match[2]
-    sum_awr2 = 0
-    sum_ckda2 = 0
-    sum_cwr2 = 0
-    for i in range(6, 11):
-        matches_played = team2[f"wins{i}"] + team2[f"losses{i}"]
-        if matches_played != 0:
-            sum_awr2 += team2[f"wins{i}"] / matches_played
 
-        if team2[f"deaths{i}"] == 0:
-            sum_ckda2 += team2[f"kills{i}"] + 0.5 * team2[f"assists{i}"]
-        else:
-            sum_ckda2 += (team2[f"kills{i}"] + 0.5 * team2[f"assists{i}"]) / team2[f"deaths{i}"]
-
-        if team2[f"c_matches{i}"] != 0:
-            sum_cwr2 += team2[f"c_wins{i}"] / team2[f"c_matches{i}"]
+    # calculate features for both teams
+    sum_awr1, sum_ckda1, sum_cwr1 = sums(team1, 1)
+    sum_awr2, sum_ckda2, sum_cwr2 = sums(team2, 6)
 
     # gather new features in a list for prediction
-    sums = [sum_awr1, sum_awr2, sum_ckda1, sum_ckda2, sum_cwr1, sum_cwr2]
-    sums = [round(num, 4) for num in sums]
+    features = [sum_awr1, sum_awr2,
+                sum_ckda1, sum_ckda2,
+                sum_cwr1, sum_cwr2]
+    # round numbers
+    features = [round(num, 4) for num in features]
 
-    return sums
+    return features
 
 
-def predict(player):
-    stats, side = live_stats(player["match"], player["id"])
-    # process match features for prediction
-    processed = summed_wr(stats)
+def get_live_match(match_id):
+    # retrieve info on live match
+    match = paladins.getMatch(match_id, True)
+    return match
+
+
+def search_player(search):
+    # attempt to find player from api
+    results = paladins.getPlayerId(search)
+    return results
+
+
+def get_status(player_id):
+    # return status for current player
+    status = paladins.getPlayerStatus(player_id)
+    return status
+
+
+def player_info(match, player_id):
+    # return account info for current player
+    for player in match:
+        if player_id == int(player['playerId']):
+            return player
+
+
+def predict_match(match):
+    # error handling: disconnected player(s)
+    if len(match) != 10:
+        return -1
+
+    # extract match stats from live match
+    stats = live_stats(match)
+    # process match features from stats for prediction
+    features = process_features(stats)
 
     # load in classifier
     filename = "model.sav"
     model = pickle.load(open(filename, "rb"))
 
     # make match predictions
-    proba = model.predict_proba([processed])
-    print("Prediction:", player["name"], "'s team has a", round(proba[0][side] * 100, 2), "chance of winning.")
-
-
-def find_player():
-    player = {}
-    while True:
-        search = input("Enter player name (or -q to quit): ")
-
-        # quit search
-        if search == "-q":
-            print("See ya later!")
-            quit()
-
-        # retrieve player from api
-        results = paladins.getPlayerId(search)
-
-        if not results:
-            print("Player ", search, " could not be found.")
-
-        elif len(results) == 1:
-            print("Player found!")
-            player["id"] = results[0]["player_id"]
-            player["name"] = results[0]["Name"]
-            break
-
-        else:
-            print(results)
-            select = int(input("Which player? "))
-            while select < 1 or select > len(results):
-                select = int(input("Choose a number between 1 and ", len(results)))
-            player["id"] = results[select - 1]["player_id"]
-            player["name"] = results[select - 1]["Name"]
-            break
-
-    # get player status for current search
-    status = paladins.getPlayerStatus(player["id"])
-    player["match"] = status["Match"]
-
-    if status["status"] == 3 and status["match_queue_id"] == 486:
-        return player
-
-    elif status["status"] == 0:
-        print(player["name"], "is offline.")
-        return find_player()
-
-    elif status["status"] == 1:
-        print(player["name"], "is in the lobby.")
-        return find_player()
-
-    else:
-        print(player["name"], "is not in a ranked match.")
-        return find_player()
-
-
-if __name__ == "__main__":
-    player = find_player()
-    predict(player)
+    proba = model.predict_proba([features])[0]
+    return proba
